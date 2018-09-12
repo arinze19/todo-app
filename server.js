fb9f9c150e2770428ef4e9689f2c69f8bb8ddcdd
arinze19 <obilaurent54@gmail.com>
2018-09-12 20:59:12 +0100

    create server

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+
+app.get('/', function (req, res){
+    res.status(200).send('Hello World');
+});
+app.get('/summer', function (req, res){
+    res.status(404).send('Page not found');
+});
+
+app.listen(port, function(){
+    console.log('server is running in on port ' + port)
+})
